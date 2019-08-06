cae79029db67044be883d8397659c61360e1a84a
Timdinia <diniahatim@gmail.com>
2019-08-06 21:29:17 +0200

    Add missing callback url for auth flow

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,7 @@
 module.exports = {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
 };
