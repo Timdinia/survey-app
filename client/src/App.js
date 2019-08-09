e5cfe1e445f0f023910b8869ed5e5ff2843cc5f4
Timdinia <diniahatim@gmail.com>
2019-08-09 17:20:18 +0200

    Add the react client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello</h1>
+                <a href="/auth/google"> Se connecter avec Google</a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
