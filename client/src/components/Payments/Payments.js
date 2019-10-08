2ec456add754b1b1a474d591b7add487787ad7f0
Tim dinia <diniahatim@gmail.com>
2019-10-08 17:44:08 +0200

    feat(billing): add checkout state management

--- client/src/components/Payments/Payments.js
+++ client/src/components/Payments/Payments.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
-export default class Payments extends Component {
+class Payments extends Component {
   render() {
     return (
       <div>
@@ -10,11 +12,16 @@ export default class Payments extends Component {
           name="Usurvey"
           description="Achat de crédits"
           amount={500}
-          token={token => console.log(token)}
-          stripeKey={process.env.REACT_STRIPE_KEY}>
+          token={token => this.props.handleToken(token)}
+          stripeKey={process.env.REACT_APP_STRIPE_KEY}>
           <Button color="inherit">Ajouter des crédits</Button>
         </StripeCheckout>
       </div>
     );
   }
 }
+
+export default connect(
+  null,
+  actions
+)(Payments);
