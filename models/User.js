5433367c9e6b84436541320b9ce005ac70e4d86b
Tim dinia <diniahatim@gmail.com>
2019-10-09 16:08:35 +0200

    feat(billing): add charge route and credits in user model

--- models/User.js
+++ models/User.js
@@ -3,6 +3,10 @@ const { Schema } = mongoose;
 
 const User = new Schema({
   googleId: String,
+  credits: {
+    type: Number,
+    default: 0,
+  },
 });
 
 module.exports = mongoose.model('users', User);
--- routes/billing.js
+++ routes/billing.js
@@ -3,12 +3,20 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('/api/stripe', async (req, res) => {
+    // check for user
+    if (!req.user) {
+      res.status(401).send({ error: 'Unauthorized' });
+    }
+    // charge the  user
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'usd',
       description: '5$ for 5 crédits',
       source: req.body.id,
     });
-    console.log(charge);
+    // add credits to the user and persist the changes
+    req.user.credits += 5;
+    const user = await req.user.save();
+    res.send(user);
   });
 };
