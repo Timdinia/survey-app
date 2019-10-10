ed4bacc97be824efb5ff4271e453c306e4f2aea6
Tim dinia <diniahatim@gmail.com>
2019-10-10 11:21:16 +0200

    feat(billing): add stripe api route to http proxy middleware

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 // Setup proxy for dev environnement
 module.exports = function(app) {
   app.use(
-    proxy(['/api/current', '/api/logout', '/auth/google'], {
+    proxy(['/api/current', '/api/logout', '/auth/google', '/api/stripe'], {
       target: 'http://localhost:8000'
     })
   );
