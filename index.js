6b68ffdb35e3c67f669d8bb5c1282da2a1418644
Tim dinia <diniahatim@gmail.com>
2019-11-05 18:26:37 +0100

    refactor(survey): add lodash chain helper

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(passport.session());
 
 // Connect to MongoDB database
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database connected'))
   .catch(err => console.log('Not connected to Database', err));
 
--- routes/survey.js
+++ routes/survey.js
@@ -14,24 +14,26 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    const events = _.map(req.body, ({ email, url }) => {
-      const pathname = new URL(url).pathname; // Extract the path from the url
-      const p = new Path('/api/surveys/:surveyId/:choice');
-      const match = p.test(pathname);
+    const p = new Path('/api/surveys/:surveyId/:choice');
 
-      if (match) {
-        return {
-          email,
-          surveyId: match.surveyId,
-          choice: match.choice,
-        };
-      }
-    });
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname); // Extract the path from the url
 
-    const compactEvents = _.compact(events);
-    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+        if (match) {
+          return {
+            email,
+            surveyId: match.surveyId,
+            choice: match.choice,
+          };
+        }
+      })
 
-    console.log(uniqueEvents);
+      .compact()
+      .uniqBy('email', 'url', 'surveyId')
+      .value();
+
+    console.log(events);
   });
 
   app.post('/api/surveys', requireAuth, requireCredits, async (req, res) => {
